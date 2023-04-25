336df2e79dc97988e09f096016a4f5587f4c7547
nsw467 <129891429+nsw467@users.noreply.github.com>
2023-04-25 17:39:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDG2mnrOkS0tafAjU1SqzXfaJS7AMKMSMg",
-  authDomain: "quora-clone-akky.firebaseapp.com",
-  projectId: "quora-clone-akky",
-  storageBucket: "quora-clone-akky.appspot.com",
-  messagingSenderId: "902318949999",
-  appId: "1:902318949999:web:8df2b97a20eece0209ef9a",
-  measurementId: "G-LJBYVXT22Z",
+  apiKey: "AIzaSyAmEZWav-dP-UwBAFgC894GYacZ5MvKq5k",
+  authDomain: "almameter-22b4f.firebaseapp.com",
+  databaseURL: "https://almameter-22b4f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "almameter-22b4f",
+  storageBucket: "almameter-22b4f.appspot.com",
+  messagingSenderId: "578719573241",
+  appId: "1:578719573241:web:4f36b8fb97ba9b259aee28",
+  measurementId: "G-W8QBSZH1TL"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
